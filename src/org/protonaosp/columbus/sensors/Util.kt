b51506e44407d93425433c67b2abba8e3fa6a7eb
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-08-02 18:33:17 +0900

    ColumbusService: Matches getMaxId method to stock
    Change-Id: I3583c9f34f96c5bc4b39a41861304186501bc4f0

--- src/org/protonaosp/columbus/sensors/Util.kt
+++ src/org/protonaosp/columbus/sensors/Util.kt
@@ -8,12 +8,16 @@ package org.protonaosp.columbus.sensors
 object Util {
     fun getMaxId(input: ArrayList<Float>): Int {
         var currentMax = -Float.MAX_VALUE
+        var i = 0
         var id = 0
-        for (i in 0 until input.size) {
+        while (i < input.size) {
+            var previousMax = currentMax
             if (currentMax < input.get(i)) {
-                currentMax = input.get(i)
+                previousMax = input.get(i)
                 id = i
             }
+            i++
+            currentMax = previousMax
         }
         return id
     }
